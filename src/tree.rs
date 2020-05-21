2fb18214add5d5fadceae15b812a9aeada54ac1c
Daniel <qiuzhanghua@icloud.com>
2020-05-21 19:19:41 +0800

    #1 cycle of try

--- src/tree.rs
+++ src/tree.rs
@@ -1,38 +1,123 @@
 use serde::{Deserialize, Serialize, Serializer};
 use std::collections::{BTreeMap, BTreeSet};
+use std::marker::PhantomData;
 
-pub trait HasKey<K, V>
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Tree<K, Item>
 where
-    K: Ord,
+    K: Eq,
+    Item: TreeNodeLike<K> + Ord,
 {
-    fn key(val: V) -> K;
+    root: Option<TreeNode<K, Item>>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
-struct Tree<K, V>
+pub trait TreeNodeLike<K>
 where
-    K: Ord,
-    V: HasKey<K, V>
+    K: Eq,
 {
-    val: V,
-    children: BTreeMap<K, V>,
+    fn key(&self) -> K;
+    fn parent(&self) -> K;
 }
 
-impl<K, V> Tree<K, V>
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+struct TreeNode<K, Item>
 where
-    K: Ord,
-    V: HasKey<K, V>,
+    K: Eq,
+    Item: TreeNodeLike<K> + Ord,
 {
-    pub fn new(val: V) -> Tree<K, V> {
-        Tree {
+    val: Item,
+    children: BTreeSet<Item>,
+    phantom: PhantomData<K>,
+}
+
+impl<K, Item> TreeNode<K, Item>
+where
+    K: Eq,
+    Item: TreeNodeLike<K> + Ord,
+{
+    pub fn new(val: Item) -> TreeNode<K, Item> {
+        TreeNode {
             val,
-            children: BTreeMap::default(),
+            children: BTreeSet::default(),
+            phantom: PhantomData,
         }
+    }
+
+    pub fn append(&mut self, val: Item) -> bool {
+        if val.parent() == self.val.key() {
+            self.children.insert(val);
+            return true;
+        }
+        false
+    }
+}
+
+impl<K, Item> Tree<K, Item>
+where
+    K: Eq,
+    Item: TreeNodeLike<K> + Ord,
+{
+    pub fn new() -> Tree<K, Item> {
+        Tree { root: None }
+    }
+
+    pub fn append(&mut self, val: Item) -> bool {
+        if self.root.is_none() {
+            self.root = Some(TreeNode::new(val));
+            return true;
+        }
+        let mut p = self.get(val.parent());
+        if val.parent() == p.as_ref().unwrap().val.key() {
+            p.as_mut().unwrap().append(val);
+            return true;
+        }
+        false
+    }
+
+    pub fn get(&mut self, key: K) -> Option<&mut TreeNode<K, Item>> {
+        self.root.as_mut()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::tree::{Tree, TreeNode, TreeNodeLike};
+    use std::cmp::Ordering;
+
     #[test]
-    pub fn test_tree_01() {}
+    pub fn test_tree_01() {
+        let mut tree = Tree::<String, Node>::new();
+        tree.append(Node {
+            key: "key".to_string(),
+            parent: "".to_string(),
+        });
+        tree.append(Node {
+            key: "key2".to_string(),
+            parent: "key".to_string(),
+        });
+        println!("{:?}", tree);
+        assert_eq!(1, 2)
+    }
+
+    #[derive(Debug, PartialOrd, PartialEq, Eq)]
+    struct Node {
+        key: String,
+        parent: String,
+    }
+
+    impl TreeNodeLike<String> for Node {
+        fn key(&self) -> String {
+            self.key.clone()
+        }
+
+        fn parent(&self) -> String {
+            self.parent.clone()
+        }
+    }
+
+    impl Ord for Node {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.key.cmp(&other.key)
+        }
+    }
 }
